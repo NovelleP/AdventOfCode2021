bbaaa0066758ad9944d08de412d5b51818f1860e
novellep <alberto109@hotmail.es>
2021-12-16 11:11:23 +0100

    day 15

---
+++ day15/day15_1.py
@@ -0,0 +1,41 @@
+def read_input(file_path: str) -> list[list[int]]:
+    with open(file_path, 'r') as fp:
+        return [[int(n) for n in line.strip()] for line in fp.readlines() if line.strip()]
+
+
+def manhattan(x1: int, y1: int, x2: int, y2: int) -> int:
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def neighbors(row: int, col: int, matrix: list[list[int]], visited: set[tuple[int, int]]) -> list[tuple[int, int]]:
+    neighs = []
+    for i in range(row - 1, row + 2):
+        if 0 <= i < len(matrix):
+            for j in range(col - 1, col + 2):
+                if (0 <= j < len(matrix[i])) and ((i, j) not in visited) and ((i, j) != (row, col)) and manhattan(row, col, i, j) == 1:
+                    neighs.append((i, j))
+    return neighs
+
+
+def search_best_path(matrix: list[list[int]], start_row: int, start_col: int, end_row: int, end_col: int) -> int:
+    distances = {(i, j): float('inf') for i in range(len(matrix)) for j in range(len(matrix[i]))}
+    distances[(start_row, start_col)] = 0
+    remaining = {(i, j): distances[(i, j)] for i in range(len(matrix)) for j in range(len(matrix[i]))}
+    visited = set()
+
+    while remaining:
+        row, col = min(remaining.items(), key=lambda t: t[1])[0]
+        del remaining[(row, col)]
+        visited.add((row, col))
+        for i, j in neighbors(row, col, matrix, visited):
+            distances[(i, j)] = min(distances[(i, j)], distances[(row, col)] + matrix[i][j])
+            remaining[(i, j)] = distances[(i, j)]
+    return distances[(end_row, end_col)]
+
+
+if __name__ == '__main__':
+
+    matrix = read_input('input.txt')
+    print(matrix)
+    ans = search_best_path(matrix, 0, 0, len(matrix) - 1, len(matrix[0]) - 1)
+    print(ans)
